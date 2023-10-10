22c63adc7df97bd5f8e0a60cf021dcd105e2761f
JereDAM <usr3593@salesianos-lacuesta.net>
2023-10-10 22:02:29 +0200

    Feat(Utils): Se añadió un metodo para contar las lineas y las palabras totales del texto

--- src/net/salesianos/utils/Utils.java
+++ src/net/salesianos/utils/Utils.java
@@ -1,5 +1,43 @@
 package net.salesianos.utils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Utils {
-    
+  public static ArrayList<String> getAllTextLineCounter(String fileRoute) {
+    ArrayList<String> TextLineCounter = new ArrayList<>();
+
+    try {
+      BufferedReader bufferedReader = new BufferedReader(new FileReader(fileRoute));
+
+      String currentLine = bufferedReader.readLine();
+
+      while (currentLine != null) {
+        TextLineCounter.add(currentLine);
+        currentLine = bufferedReader.readLine();
+      }
+
+      bufferedReader.close();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    return TextLineCounter;
+  }
+
+  public static Integer getTotalWordsFrom(String fileRoute) {
+    try {
+      BufferedReader bufferedReader = new BufferedReader(new FileReader(fileRoute));
+      String firsLine = bufferedReader.readLine();
+      bufferedReader.close();
+      return Integer.parseInt(firsLine);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return 0;
+  }
 }
